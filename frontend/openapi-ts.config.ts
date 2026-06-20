aab372051848e082ac5b59249abf91e1b58284d2
CJ Rosales <gargoth13@gmail.com>
2026-06-20 13:28:50 +1200

    chore(fmt): Run `npm run format`

--- frontend/openapi-ts.config.ts
+++ frontend/openapi-ts.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from '@hey-api/openapi-ts';
+import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-  input: '../openapi/bundled.yaml',
-  output: './src/api',
-  plugins: [
-    '@hey-api/client-fetch',
-    '@tanstack/react-query',
-  ],
+    input: "../openapi/bundled.yaml",
+    output: "./src/api",
+    plugins: ["@hey-api/client-fetch", "@tanstack/react-query"],
 });
